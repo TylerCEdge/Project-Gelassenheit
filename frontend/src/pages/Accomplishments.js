2dadf09ec7c18d5b973308391a5ce2e732bb0c6a
TylerCEdge <tylercedge@gmail.com>
2020-08-15 19:21:47 -0400

    added an accomplishment to the accomplishment page

--- frontend/src/pages/Accomplishments.js
+++ frontend/src/pages/Accomplishments.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import Container from '../components/Container';
+
 const Accomplishments = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <Container>
+        <hr />
+        <ul className='journal'>
+          <li className='journal-entry'>
+            <p>I made this by myself!!!</p>
+          </li>
+        </ul>
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Accomplishments;

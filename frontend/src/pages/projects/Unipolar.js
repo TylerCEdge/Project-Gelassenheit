9f2b1f69d88716895f27372ea2d091e4714907ad
TylerCEdge <tylercedge@gmail.com>
2020-08-16 18:50:17 -0400

    added two new micro projects.

---
+++ frontend/src/pages/projects/Unipolar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Unipolar = () => {
+  return <div></div>;
+};
+
+export default Unipolar;
+
+// Render my image from the about section with a caption that asks if I am awesome.
+// Render two buttons: Yes and No.
+// Yes can be clicked and adds something to the page (I haven't figured that part out).
+// No can't be clicked.  Ideally id like to create an invisible margin around it that triggers the button to move at random around the screen when the cursor enters that parameter.
+// Add an array of random generated taunts to be produced from an array for bonus points!  Taunts to be triggered on each attempt to enter the No button area.  Taunts to be rendered at random as replacement text for the button.

5c134e51752dc0b68a7882c718ea2137c433e168
TylerCEdge <tylercedge@gmail.com>
2020-08-19 19:13:53 -0400

    started working on project unipollar.  I still need to work on the No button functionality.  Idea is to have the no button run away from the cursor!

--- frontend/src/pages/projects/Unipollar.js
+++ frontend/src/pages/projects/Unipollar.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { AbsoluteCenter } from '../../components/Positioning/AbsoluteCenter';
 import '../../css/TextStyles.css';
 
 const Unipollar = () => {
-  return <h2 className='color-mod text-center'>Is Tyler Awesome?</h2>;
+  const [count, setCount] = useState(0);
+
+  function onClickHandler() {
+    setCount((count) => count + 1);
+  }
+  return (
+    <AbsoluteCenter>
+      <h2 className='color-mod text-center'>Is Tyler Awesome?</h2>
+      <h4 className='color-mod'>{count}</h4>
+      <div className='btn-group'>
+        <button
+          className='btn btn-primary'
+          id='awesome'
+          onClick={onClickHandler}
+        >
+          Yes
+        </button>
+
+        <button className='btn btn-danger' id='notAwesome'>
+          No
+        </button>
+      </div>
+    </AbsoluteCenter>
+  );
 };
 
 export default Unipollar;
